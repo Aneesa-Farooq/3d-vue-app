66c575a76b5cc0d6867d7e5151e5744bbc3d8a40
Aneesa-Farooq <aneesa.farooq2000@gmail.com>
2023-03-19 17:07:03 +0000

    fix few issues

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Gown from "./Views/Gown.vue"
+import Gown from "./Views/gown.vue"
 import ShalwarKameez from "./Views/shalwarKameez.vue"
 import MeshFrock from "./Views/meshFrock.vue"
 import Kameez from "./Views/kameez.vue"
 import ShortFrock from "./Views/shortFrock.vue"
-import BallGown from "./Views/ballGown.vue"
 
 const routes = [
   {
@@ -27,11 +26,6 @@ const routes = [
     component:Kameez ,
     name:"Kameez"
   },
-  {
-      path: "/ballGown",
-      component:BallGown ,
-      name:"BallGown"
-    },
   {
     path: "/shortFrock",
     component:ShortFrock ,
