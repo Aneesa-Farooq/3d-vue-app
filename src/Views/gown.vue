f86d0ae85219c64aca59bd60d097136214531724
Alishbaiftikhar <alishbaiftikhar2001@gmail.com>
2023-05-24 15:33:58 +0500

    change in gown

--- src/Views/gown.vue
+++ src/Views/gown.vue
@@ -231,7 +231,7 @@ export default {
               }
             }
           });
-        }
+        }        
         async function store() {
           console.log(scene);
           const exporter = new GLTFExporter();
